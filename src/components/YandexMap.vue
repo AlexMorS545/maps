6ad93121db2f71fa932cc0a4677c7545a87c7687
AlexMorS545 <morsan20@yandex.ru>
2021-05-15 15:05:26 +0300

    start project

---
+++ src/components/YandexMap.vue
@@ -0,0 +1,38 @@
+<template>
+  <yandex-map 
+    :coords="coords"
+    :zoom="10" 
+    :settings="settings"
+  >
+    <ymap-marker 
+      :coords="coords" 
+      marker-id="123" 
+      hint-content="some hint" 
+    />
+  </yandex-map>
+</template>
+<script>
+import { yandexMap, ymapMarker } from 'vue-yandex-maps'
+
+export default {
+  name: 'Map',
+  data: () => ({
+    coords: [
+      54.82896654088406,
+      39.831893822753904,
+    ],
+    settings: {
+      apiKey: '6025a482-aac7-49c7-92f3-80a120e27659',
+      lang: 'ru_RU',
+      coordoder: 'latlong',
+      version: '2.1'
+    }
+  }),
+  components: {yandexMap, ymapMarker},
+  methods: {
+    onClick(e) {
+      this.coords = e.get('coords');
+    },
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Hello</h2>
+    <Cities/>
+    <Map />
+  </div>
+</template>
+
+<script>
+import Cities from '@/components/Cities'
+import Map from '@/components/YandexMap'
+
+export default {
+  name: 'Home',
+  components: {Cities, Map}
+}
+</script>
